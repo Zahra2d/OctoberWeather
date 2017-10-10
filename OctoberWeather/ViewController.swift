e04fcee22b418a153c812d02284728219bc6f8af
Zahraa Darwish <zahraadarwish91@gmail.com>
2017-10-10 20:49:32 +0300

    added the tableView :D

--- OctoberWeather/ViewController.swift
+++ OctoberWeather/ViewController.swift
@@ -8,14 +8,30 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDataSource {
 
+    @IBOutlet weak var tableView: UITableView!
+    let cellId = "cell"
+    let names: [String] = ["Hussein", "Zahraa", "Baby", "Pinky"]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
-    }
-
+        tableView.dataSource = self
     
+    }
+    
+    // MARK: - UITableViewDataSourceemehhr
+    
+
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return names.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: cellId)
+        cell?.textLabel?.text = names[indexPath.row]
+        return cell!
+    }
 
 
 }
